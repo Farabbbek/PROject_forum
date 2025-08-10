1fc96c2bf64d676fc33fb6bf99db1e28b4a4226c
atusup <ftusup@bk.ru>
2025-08-11 01:50:49 +0300

    blog

---
+++ web/static/js/login.js
@@ -0,0 +1,11 @@
+const container = document.getElementById('container')
+const registrBtn = document.getElementById('register')
+const loginBtn = document.getElementById('login')
+
+registrBtn.addEventListener('click', () => {
+    container.classList.add("active");
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
+});
